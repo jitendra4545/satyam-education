5b75b452f87d6f2108c3315b66b7d467732e888f
jitendra4545 <jitendra45.js45@gmail.com>
2023-08-10 18:33:13 +0530

    added

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 
 
 const express=require(`express`)
+const cors=require('cors')
 const { connection } = require("./config/db")
+const { ResultRouter } = require("./routes/ResultRoute")
 require("dotenv").config()
 const app=express()
+const bodyParser=require('body-parser')
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(cors())
 
-
+app.use("/result",ResultRouter)
 app.get("/",(req,res)=>{
     console.log('welcome to satyam education')
 })
--- model/ResultModel.js
+++ model/ResultModel.js
@@ -3,39 +3,44 @@ const { default: mongoose } = require("mongoose");
 
 
 
-const ResultSchema=mongoose.Schema({
-    image: {
-        data: Buffer,
-        contentType: String
-    },
-    session:{type:String},
-    roll_no:{type:String},
-    student_name:{type:String},
-    fathers_name:{type:String},
-    mothers_name:{type:String},
-    date_of_birth:{type:String},
-    gender:{type:String},
-    admission_in_class:{type:String},
-    course_name:{type:String},
-    sub1:{type:String},
-    mark1:{type:Number},
-    sub2:{type:String},
-    mark2:{type:Number},
-    sub3:{type:String},
-    mark3:{type:Number},
-    sub4:{type:String},
-    mark4:{type:Number},
-    sub5:{type:String},
-    mark5:{type:Number},
-    sub6:{type:String},
-    mark6:{type:Number},
-    sub7:{type:String},
-    mark7:{type:Number},
-    sub8:{type:String},
-    mark8:{type:Number},
+const ResultSchema = mongoose.Schema({
+    image: {data:Buffer,contentType:String },
+    session: { type: String },
+    roll_no: { type: String },
+    student_name: { type: String },
+    fathers_name: { type: String },
+    mothers_name: { type: String },
+    date_of_birth: { type: String },
+    gender: { type: String },
+    admission_in_class: { type: String },
+    course_name: { type: String },
+    sub1: { type: String },
+    mark1: { type: Number },
+    sub2: { type: String },
+    mark2: { type: Number },
+    sub3: { type: String },
+    mark3: { type: Number },
+    sub4: { type: String },
+    mark4: { type: Number },
+    sub5: { type: String },
+    mark5: { type: Number },
+    sub6: { type: String },
+    mark6: { type: Number },
+    sub7: { type: String },
+    mark7: { type: Number },
+    sub8: { type: String },
+    mark8: { type: Number },
 
 
-},{
-    versionKey:false,
-    timestamps:true
+}, {
+    versionKey: false,
+    timestamps: true
 })
+
+const ResultModel = mongoose.model("result", ResultSchema)
+
+
+
+module.exports = {
+    ResultModel
+}
