6bcc23da3af3ead147878e3c4b2916a230e44876
jitendra4545 <jitendra45.js45@gmail.com>
2023-08-18 16:56:25 +0530

    added

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const { ImageModel } = require('./model/TestModel')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(cors())
-app.use("/admission",StudentRouter)
+// app.use("/admission",StudentRouter)
 app.use("/result",ResultRouter)
 app.get("/",(req,res)=>{
     console.log('welcome to satyam education')
