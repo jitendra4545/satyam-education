bf37cadbd27d6f61bd7afb524d84d3294ef6ff50
jitendra4545 <jitendra45.js45@gmail.com>
2023-08-18 10:02:08 +0530

    added

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { StudentRouter } = require('./routes/StudentAdmissionRoute')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use(cors())
-app.use("/admission",StudentRouter)
+// app.use("/admission",StudentRouter)
 app.use("/result",ResultRouter)
 app.get("/",(req,res)=>{
     console.log('welcome to satyam education')
